f1e0848a151c7d1f2d9cabe964e12900d972ab0e
Henry Miskin <henry.miskin@talktalk.net>
2014-12-10 00:56:24 +0000

    Merged to ammend url to relative references

--- resources/js/services/devices.js
+++ resources/js/services/devices.js
@@ -5,7 +5,7 @@ app.service("DevicesService",['$resource',function($resource){
 
 app.provider('DevicesProvider', function(){
   this.$get = ['$resource', function($resource) {
-    var DevicesProvider = $resource('//127.0.0.1:80/devices/:deviceID',{},{
+    var DevicesProvider = $resource('/devices/:deviceID',{},{
       update: {
         method: 'PUT'
       }
--- resources/js/services/lights.js
+++ resources/js/services/lights.js
@@ -5,7 +5,7 @@ app.service("LightsService",['$resource',function($resource){
 
 app.provider('LightsProvider', function(){
   this.$get = ['$resource', function($resource) {
-    var LightsProvider = $resource('//127.0.0.1:80/lights/:lightID',{},{
+    var LightsProvider = $resource('/lights/:lightID',{},{
       update: {
         method: 'PUT'
       }
